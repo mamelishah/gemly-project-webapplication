41dfa32658870fafe36d63835acf5bc1eb8d1346
mamelishah <eaa25mkaal@students.eaaa.dk>
2026-05-27 20:49:02 +0200

    Tilføjet typografien fra Google Fonts og importeret den ind i Main, og indsat den ind i global styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+/* Importing the global styles */
+import './styles/global.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
