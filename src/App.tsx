44799111cb824100d3dbfc4f72cf538abfdb75fa
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-05-28 15:31:16 +0200

    Tilføjet PlanToTravelCard komponent med tilhørende CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css";
+import PlanToTravelCard from "./components/cards/PlanToTravelCard";
 import SearchBar from "./components/ui/input/SearchBar";
+
 
 function App() {
   return (
     <>
       <SearchBar showCancelIcon={true} 
       placeholderText={"Søg efter steder..."} />
-    </>
+
+      <PlanToTravelCard />
+
+   </>
+
   );
 }
 
