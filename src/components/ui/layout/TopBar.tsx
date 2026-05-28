370442174912b922a43cafa644c2d41a17e95036
Julie Wendelboe Bøcher <juliewboecher@gmail.com>
2026-05-28 10:47:25 +0200

    Tilføjet TopBar komponent med layout og ikoner

---
+++ src/components/ui/layout/TopBar.tsx
@@ -0,0 +1,11 @@
+function TopBar() {
+    return (
+        <section id="top-bar">
+            <img src="arrowLeftBig-icon.svg" alt="Arrow Left" />
+            <h1>Top Bar</h1>
+            <img src="cancelBig-icon.svg" alt="Close" />
+        </section>
+    );
+}
+
+export default TopBar;
