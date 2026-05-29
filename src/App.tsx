112a00229325e1ee68e9a55de43173648fac6a82
mamelishah <eaa25mkaal@students.eaaa.dk>
2026-05-29 11:30:54 +0200

    tilføjet routing i app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 
 /* Importing the pages */
 import Explore from './pages/Explore';
 import Map from './pages/Map';
+import AccountPosted from './pages/AccountPosted';
+import Filter from './pages/Filter';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
+import Post from './pages/Post';
+import Onboarding from './pages/Onboarding';
+import Preboarding from './pages/Preboarding';
 import Account from './pages/Account';
 
 
 function App() {
   return (
     <>
+      <Routes>
+      <Route path="/" element={<Explore />} />
+      <Route path="/map" element={<Map />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/account-posted" element={<AccountPosted />} />
+      <Route path="/filter" element={<Filter />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/saved" element={<Saved />} />
+      <Route path="/post" element={<Post />} />
+      <Route path="/onboarding" element={<Onboarding />} />
+      <Route path="/preboarding" element={<Preboarding />} />
+    </Routes>
     </>
   );
 }
