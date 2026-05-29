eb16b7ce697afe8a8552a63eccdcc4f58b018124
mamelishah <eaa25mkaal@students.eaaa.dk>
2026-05-29 11:08:57 +0200

    tilføjet templates til pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Routes } from 'react-router-dom';
 import './App.css'
+import TopBar from './components/ui/layout/TopBar';
 import BottomNavigationBar from './components/ui/layout/bottom-navigation-bar/BottomNavigationBar.tsx';
+
+/* Importing the pages */
+import Explore from './pages/Explore';
+import Map from './pages/Map';
+import Account from './pages/Account';
+
 
 function App() {
 
   return (
     <>
-      <BottomNavigationBar/>
+       <Routes>
+      <Route path="/" element={<Explore />} />
+      <Route path="/map" element={<Map />} />
+      <Route path="/account" element={<Account />} />
+    </Routes>
     </>
   )
 }
