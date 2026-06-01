7d60a41e822bdd0ecd1ecb8a446796930f8f14d1
cecilieskals <eaa25cts@students.eaaa.dk>
2026-06-01 18:10:01 +0200

    Added ProfileCard component and updated Account page layout

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
 import BottomNavigationBar from '../components/ui/layout/bottom-navigation-bar/BottomNavigationBar';
+import ProfileCard from '../components/cards/profileCard/ProfileCard';
+import TopBar from '../components/ui/layout/TopBar';
+import "./Account.css"
+import editIcon from "/src/assets/icons/EditAndDelete/edit-icon.svg"
 
 function Account() {
   return (
     <>
-    <BottomNavigationBar/>
+    <div className="account-page">
+      <TopBar title="Din profil" showLeftIcon={false} showRightIcon={false} />
+
+      <div id="edit-profile-button">
+        <img src={editIcon} alt="Edit Icon" />
+        <h4>Administrer din profil</h4>
+      </div>
+
+      <ProfileCard title="Dine gemte" showBackIcon={true} />
+      <ProfileCard title="Dine opslag" showBackIcon={true} />
+      <ProfileCard title="Cookie indstillinger" showBackIcon={true} />
+      <ProfileCard title="Support" showBackIcon={true} />
+      <ProfileCard title="Log af" showBackIcon={false} />
+
+      <BottomNavigationBar />
+    </div>
     </>
-  )
+  );
 }
 
 export default Account
