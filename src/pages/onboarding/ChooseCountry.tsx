bc54d4044cc50853528051b46c4f02fa5fc703bd
mamelishah <eaa25mkaal@students.eaaa.dk>
2026-06-02 11:26:48 +0200

    Tilføjet de sidste ting i for onboarding

--- src/pages/onboarding/ChooseCountry.tsx
+++ src/pages/onboarding/ChooseCountry.tsx
@@ -2,11 +2,34 @@ import { useState } from "react";
 import "./onboarding.css";
 import "./CreatePassword.css";
 import "./chooseCountry.css";
-import SearchBar from "../../components/ui/input/SearchBar";
 import searchIcon from "/src/assets/icons/navbar/search-icon.svg";
 
+// 1. Lav en liste med alle landene og deres flag-koder
+const COUNTRIES_DATA = [
+  { name: "Belgien", code: "be" },
+  { name: "Danmark", code: "dk" },
+  { name: "Finland", code: "fi" },
+  { name: "Frankrig", code: "fr" },
+  { name: "Grækenland", code: "gr" },
+  { name: "Holland", code: "nl" },
+  { name: "Irland", code: "ie" },
+  { name: "Italien", code: "it" },
+  { name: "Norge", code: "no" },
+  { name: "Polen", code: "pl" },
+  { name: "Portugal", code: "pt" },
+  { name: "Spanien", code: "es" },
+  { name: "Sverige", code: "se" },
+  { name: "Tyskland", code: "de" },
+  { name: "Østrig", code: "at" },
+];
+
 function ChooseCountry() {
-   const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // 2. Filtrer listen live baseret på hvad der skrives i input-feltet
+  const filteredCountries = COUNTRIES_DATA.filter((country) =>
+    country.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <div className="onboarding-page">
@@ -14,9 +37,8 @@ function ChooseCountry() {
         <div className="text-container-onboarding">
           <h1 className="slide-title">Hvor kommer du fra?</h1>
           <h2 className="slide-subtitle"></h2>
-          <p className="slide-infobox">Vælg det land du bor i.</p>
+          <p className="slide-infobox">Vælg hvilke lande du vil udforske</p>
         </div>
-
 
         <div className="search-container">
           <div className="searchbar-container">
@@ -25,86 +47,26 @@ function ChooseCountry() {
               id="search-input-country"
               name="search"
               value={searchQuery}
-              placeholder="Søg efter dit land"
+              placeholder="Søg..."
               onChange={(e) => setSearchQuery(e.target.value)}
             />
           </div> 
 
           <div className="country-list">
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/be.png" alt="Belgien" />
-              <p>Belgien</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/dk.png" alt="Danmark" />
-              <p>Danmark</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/fi.png" alt="Finland" />
-              <p>Finland</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/fr.png" alt="Frankrig" />
-              <p>Frankrig</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/gr.png" alt="Grækenland" />
-              <p>Grækenland</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/nl.png" alt="Holland" />
-              <p>Holland</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/ie.png" alt="Irland" />
-              <p>Irland</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/it.png" alt="Italien" />
-              <p>Italien</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/no.png" alt="Norge" />
-              <p>Norge</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/pl.png" alt="Polen" />
-              <p>Polen</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/pt.png" alt="Portugal" />
-              <p>Portugal</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/es.png" alt="Spanien" />
-              <p>Spanien</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/se.png" alt="Sverige" />
-              <p>Sverige</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/de.png" alt="Tyskland" />
-              <p>Tyskland</p>
-            </div>
-
-            <div className="country-item">
-              <img src="https://flagcdn.com/w40/at.png" alt="Østrig" />
-              <p>Østrig</p>
-            </div>
+            {/* 3. Hvis der er match, så loop igennem de filtrerede lande */}
+            {filteredCountries.length > 0 ? (
+              filteredCountries.map((country) => (
+                <div className="country-item" key={country.code}>
+                  <img src={`https://flagcdn.com/w40/${country.code}.png`} alt={country.name} />
+                  <p>{country.name}</p>
+                </div>
+              ))
+            ) : (
+              /* Hvis brugeren søger efter noget sludder, der ikke findes på listen: */
+              <p style={{ padding: "10px 0", color: "#888", fontStyle: "italic" }}>
+                Ingen lande fundet...
+              </p>
+            )}
           </div> 
         </div> 
       </div> 
